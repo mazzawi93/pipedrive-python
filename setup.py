ab047e12f2b2286bb9359cb0452629be47c464bd
Anthony Mazzawi <anthonymazzawi@gmail.com>
2023-03-10 16:52:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description="API wrapper for Pipedrive written in Python",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    url="https://github.com/GearPlug/pipedrive-python",
+    url="https://github.com/mazzawi93/pipedrive-python",
     author="Miguel Ferrer",
     author_email="ingferrermiguel@gmail.com",
     license="MIT",
